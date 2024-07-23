3cc6d0d9d981fff18a26948ced020ba65f75c742
Ali Ahmad <143416088+aliahmad884@users.noreply.github.com>
2024-07-23 18:59:22 +0000

    temp

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.SQL_DB_NAME, process.env.SQL_USER, process.env.SQL_PASSWORD, {
     host: process.env.SQL_HOST,
     dialect: "mysql",
-    port: process.env.SQL_PORT || 3306, // Ensure port is provided or defaults to 3306
-});
+    port: process.env.SQL_PORT || 3306, // Ensure is provided or defaults to 3306
+}); 
 
 sequelize.authenticate()
     .then(() => {
